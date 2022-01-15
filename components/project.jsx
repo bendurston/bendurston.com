24e23d1a7163d4a71d6cd74ef6f786fbc4c1b6db
Ben Durston <bengdurston@gmail.com>
2022-01-15 15:20:09 -0500

    Added basic transition to project component.

--- components/project.jsx
+++ components/project.jsx
@@ -4,6 +4,9 @@ import Link from "next/link";
 // Component imports
 import Technology from "./technology";
 
+import { Transition } from "@headlessui/react";
+
+
 export default function Project({
   title,
   description,
@@ -12,14 +15,25 @@ export default function Project({
   link,
   linkText
   }) {
-  //md:max-w-md max-w-sm 
   return (
-    <div className="lg:basis-5/12 basis-1/1 bg-red-100 lg:m-3 p-10 mx-4 my-3">
-      <p>{title}</p>
-      <Technology technologies={technologies} />
-      <p>{inDevelopment}</p>
-      <p>{description}</p>
-      <a href={link}>{linkText}</a>
-    </div>
+      <Transition
+        as="div"
+        appear={true}
+        show={true}
+        enter="transform transition duration-[400ms]"
+        enterFrom="opacity-0 rotate-[-120deg] scale-50"
+        enterTo="opacity-100 rotate-0 scale-100"
+        leave="transform duration-200 transition ease-in-out"
+        leaveFrom="opacity-100 rotate-0 scale-100 "
+        leaveTo="opacity-0 scale-95 "
+      >
+        <div className="lg:basis-5/12 basis-1/1 bg-red-100 lg:m-3 p-10 mx-4 my-3">
+          <p>{title}</p>
+          <Technology technologies={technologies} />
+          <p>{inDevelopment}</p>
+          <p>{description}</p>
+          <a href={link}>{linkText}</a>
+        </div>
+      </Transition>
   );
 }
